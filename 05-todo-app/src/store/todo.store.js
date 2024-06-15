27a775fdaf4d83a9297419fba0e283701b4f0b35
Byron Machin <bmachin7@gmail.com>
2024-06-15 12:37:17 -0500

    Logica para mostrar los todo en pantalla.

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -10,7 +10,10 @@ const state = {
     todos: [
         new Todo('Soul Stone'),
         new Todo('Reality Stone'),
-        new Todo('Time Stone')
+        new Todo('Time Stone'),
+        new Todo('Mind Stone'),
+        new Todo('Power Stone'),
+        new Todo ('Space Stone')
     ],
     filter: Filters.All
 }
@@ -20,6 +23,74 @@ const initStore = () => {
     console.log('initStore 🥑');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const getTodos = ( filter = Filters.All ) => {
+    switch ( filter ) {
+        case Filters.All:
+            return state.todos;
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+        case Filters.Pending:
+            return state.todos.filter( todo =>!todo.done );
+        default:
+            throw new Error(`Invalid filter: ${filter}`);
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    if (!description) throw new Error('Description is required');
+
+    state.todos.push( new Todo(description) );
+}
+
+/**
+ * 
+ * @param {String} todoId
+ */
+const toggleTodo = ( todoId ) => {
+    state.todos = state.todos.map( todo => {
+        if (todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+}
+
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter(todo => todo.id !== todoId );
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => todo.done );
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo
 }
--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -1,4 +1,11 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store';
+import { renderTodos } from './use-cases';
+
+const ElementIDs = {
+    TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
+}
 
 /**
  * 
@@ -7,10 +14,47 @@ import html from './app.html?raw';
 
 export const App = (elementId) => {
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos( ElementIDs.TodoList, todos );
+    }
+
     // When app function is called
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
+
+    // HTML reference
+    const newDescriptionInput = document.querySelector( ElementIDs.NewTodoInput );
+    const todoListUl = document.querySelector( ElementIDs.TodoList );
+    
+
+    // Listeners
+    newDescriptionInput.addEventListener('keyup', (e) => {
+      if (e.keyCode != 13) return;
+      if (e.target.value.trim().length === 0) return;
+
+      todoStore.addTodo( e.target.value );
+      displayTodos();
+      e.target.value = '';
+    });
+
+    todoListUl.addEventListener('click', (e) => {
+      const selectedElement = e.target.closest('[data-id]');
+      todoStore.toggleTodo( selectedElement.getAttribute('data-id') );
+      displayTodos();
+    });
+
+    todoListUl.addEventListener('click', (e) => {
+      const isDestroyElement = e.target.className === 'destroy';
+      const selectedElement = e.target.closest('[data-id]');
+
+      if ( !e || !isDestroyElement ) return;
+      todoStore.deleteTodo( selectedElement.getAttribute('data-id') );
+      displayTodos();
+    });
+    
 }
