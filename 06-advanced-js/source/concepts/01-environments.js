2a11076ee62c18cf68d4ee7b604ed4351b7f1641
Byron Machin <bmachin7@gmail.com>
2024-06-21 16:56:54 -0500

    Callbacks, promises and HELL

---
+++ 06-advanced-js/source/concepts/01-environments.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+
+export const environmentsComponent = (element) => {
+  console.log(import.meta.env);
+
+  const html = `
+      variables:
+`;
+  element.innerHTML = html;
+};
